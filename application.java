c2453149c0abd46f0ef0a5191f3f1f83e5ae8252
shyam <shyam.engg03@gmail.com>
2024-10-29 01:29:47 +0530

    ethan change the java file

--- application.java
+++ application.java
@@ -25,3 +25,12 @@ public class SumCalculator {
 }
 
 }
+
+#ethen change the java code
+
+// loadClass function prototype
+
+Class r = loadClass(String className, boolean resolveIt);
+
+// className: name of the class to be loaded
+// resolveIt: flag to decide whether any referenced class should be loaded or not.
